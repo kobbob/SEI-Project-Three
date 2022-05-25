0a8074fbbdc35901579e0b6f7492e5248a70d195
kobbok <kate.oboyle@icloud.com>
2022-05-25 15:12:55 +0100

    added-to-do-list-on-about-page

--- nike-mern-app/client/src/components/About.js
+++ nike-mern-app/client/src/components/About.js
@@ -6,12 +6,15 @@ const About = () => {
       <h1>About Page</h1>
       <p> Things I didn&apos;t manage to achieve so far:
         <ul>
+          <li>Front-end:</li>
           <li>Better Welcome Page;</li>
           <li>Offset/Staggered Grid;</li>
           <li>Responsive Design;</li>
           <li>Filtering collaborations - existing to future;</li>
           <li>Cloudinary - uploading images + form;</li>
           <li>Animated cursor;</li>
+          <li>Navigate between pop-up single collabs.</li>
+          <li>Add, Edit, Delete Collab -- Add in cloudinary functions</li>
         </ul>
       </p>
       <h2>Initial Moodboard</h2>
